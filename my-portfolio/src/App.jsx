23e9ec3c1abdb6def8f6906bc56d176b12144180
kakotodev <vinh-lam.le@efrei.net>
2025-06-15 20:25:01 +0200

    création des fichiers basiques
    npm

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple portfolio application.</p>
+    </div>
+  );
+}
+
+export default App;
