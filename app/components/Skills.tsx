a9ce8dfad6972f3cf25b50c7f27406757ab2fada
Kakoto <vinhlam.le29@gmail.com>
2026-01-18 17:18:57 +0100

    Ajout de Skills.tsx

---
+++ app/components/Skills.tsx
@@ -0,0 +1,9 @@
+export default function Skills() {
+    return(
+        <>
+            <div>
+                <h1>Fonctionnalités à venir</h1>
+            </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import About from "./components/About";
+import Skills from './components/Skills';
 
 export default function Home() {
   return (
     <main>
       <About />
+      <Skills />
     </main>
   );
 }
