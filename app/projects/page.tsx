b35313102c18986c1a4f3b79904a0ee3370cfc91
kakotodev <vinh-lam.le@efrei.net>
2026-01-21 17:30:05 +0100

    Ajout des projets dans la page mes projets

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,17 @@
+import ProjectCard from '../components/card/ProjectCard';
+import { listProjects } from '../data/projects';
+
 export default function MyProjects() {
     return (
         <main>
-            <h1>Mes projets : </h1>
+            <div>
+                <h2>Mes projets : </h2>
+            </div>
+            <div className="flex justify-center">
+                    {listProjects.map((project) => (
+                        <ProjectCard  key={project.id} listProjects={project} />
+                    ))}
+            </div>
         </main>
     )
 }
