9a8092c03cac1373c4ee350c73e9cedb45eda163
Kakoto <vinhlam.le29@gmail.com>
2026-01-22 22:03:54 +0100

    badge

--- app/components/card/ProjectCard.tsx
+++ app/components/card/ProjectCard.tsx
@@ -1,8 +1,10 @@
 import { Project } from '../../data/data-projects';
+import { techColors } from '../../data/data-projects';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function ProjectCard({ listProjects }: { listProjects: Project}) {
+
     return(
         <>
             <div className='hidden text-center mx-7 my-5 w-[400px] h-[560px] border lg:block'>
@@ -12,10 +14,19 @@ export default function ProjectCard({ listProjects }: { listProjects: Project}) 
                     <div className='h-[80px]'>
                         <h3>{listProjects.title}</h3>
                     </div>
-                    <div className='text-center mb-5 h-[40px]'>
-                        {listProjects.technologies.map((tech, index) => (
-                            <span key={index}>{tech}{index < listProjects.technologies.length - 1 && " "}</span>
-                        ))}
+                    <div className='flex justify-center'>
+                        <div className='flex flex-wrap justify-center mb-5 h-[80px] w-[350px]'>
+                            {listProjects.technologies.map((tech, index) => {
+                                
+                                const colorBadgeTech = techColors[tech] || 'bg-gray-500';
+
+                                return (
+                                    <div key={index} className={`h-[20px] px-1 pb-[25px] mx-2 border-2 rounded-full ${colorBadgeTech}`}>
+                                        <span>{tech}</span>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                     <div className='text-[18px] h-[100px] italic'>
                         <p>{listProjects.description}</p>
--- app/data/data-projects.tsx
+++ app/data/data-projects.tsx
@@ -9,6 +9,17 @@ export interface Project {
     urlImg: string;
 }
 
+export const techColors: Record<string, string> = {
+    "Vue.JS": "border-green-500 bg-green-500/15",
+    "FastAPI": "border-blue-500 bg-blue-500/15",
+    "Docker": "border-blue-700 bg-blue-700/15",
+    "TailwindCSS": "border-teal-400 bg-teal-400/15",
+    "PostegreSQL": "border-indigo-600 bg-indigo-600/15",
+    "Git": "border-orange-500 bg-orange-500/15",
+    "PHP": "border-purple-600 bg-purple-600/15",
+    "Twig": "border-pink-400 bg-pink-400/15"
+};
+
 export const listProjects: Project[] = [    
     {
         id: 1,
