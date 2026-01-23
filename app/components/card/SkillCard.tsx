f2f5552fa19582764b623c008de7e9604fc637a6
kakotodev <vinh-lam.le@efrei.net>
2026-01-23 08:21:50 +0100

    ajout de l'infobull

--- app/components/card/SkillCard.tsx
+++ app/components/card/SkillCard.tsx
@@ -20,8 +20,12 @@ export default function SkillCard() {
                     <div className="flex justify-center">
                         {listSkills.map((skill) => (
                             skill.urlImg ? (
-                                <div key={skill.id} className="flex justify-center">
+                                <div key={skill.id} className="flex relative group justify-center hover:grayscale-0 mx-2">
                                     <Image src={skill.urlImg} alt="skill" width={40} height={40}/>
+                                    <span className="absolute scale-0 transition-all rounded group-hover:scale-100 -top-10 shadow-xl">
+                                        {skill.name}
+                                        <span className="absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-800"></span>
+                                    </span>
                                 </div>
                             ) : null
                         ))}
