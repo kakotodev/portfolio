0ea06a4ce867a4614ceb1d110f8e1f29b23550c8
Kakoto <vinhlam.le29@gmail.com>
2026-01-24 23:42:53 +0100

    Creation de data-soft-skills

---
+++ app/data/data-soft-skills.tsx
@@ -0,0 +1,9 @@
+interface SoftSkills {
+    id: number;
+    name: string;
+}
+
+export const listSoftSkills: SoftSkills[] = [
+    { id: 1, name: "Travail en équipe" },
+    { id: 2, name: "Communication" },
+],
