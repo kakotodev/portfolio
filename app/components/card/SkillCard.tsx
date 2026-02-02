4dc55f4989919b1ef6e90de6584674efbff01cdb
kakotodev <vinh-lam.le@efrei.net>
2026-02-02 23:02:35 +0100

    changement de balise de Image à img

--- app/components/card/SkillCard.tsx
+++ app/components/card/SkillCard.tsx
@@ -1,6 +1,5 @@
 import { listSkills } from "@/app/data/data-skills";
 import { Skill } from "@/app/data/data-skills";
-import Image from "next/image";
 
 export default function SkillCard() {
 
@@ -25,7 +24,7 @@ export default function SkillCard() {
                                 {listSkills.map((skill) => (
                                     skill.urlImg ? (
                                         <div key={skill.id} className="flex relative group justify-center lg:hover:grayscale-0 z-0 mx-5 my-3 grayscale-0 lg:grayscale transition-all">
-                                            <Image src={skill.urlImg} alt="skill" width={40} height={40} className="h-[40px]"/>
+                                            <img src={skill.urlImg} alt="skill" width={40} height={40} className="h-[40px]"/>
                                             <span className="absolute lg:scale-0 transition-all rounded lg:group-hover:scale-100 top-10 lg:-top-10 shadow-xl">
                                                 {skill.name}
                                                 <span className="hidden lg:block absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-800"></span>
